fb20ca0e157db35fab4301f30402b86207f49999
fredkendy <fredkendy9@gmail.com>
2024-01-10 20:56:53 -0300

    setup inicial

---
+++ src/app/pages/tipo-servicos/tipo-servicos-listagem/tipo-servicos-listagem.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-servicos-listagem',
+  templateUrl: './tipo-servicos-listagem.page.html',
+  styleUrls: ['./tipo-servicos-listagem.page.scss'],
+})
+export class TipoServicosListagemPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
