53aa542832c27e974b125b785158feba43df2859
fredkendy <fredkendy9@gmail.com>
2024-01-17 15:12:46 -0300

    insercao de ordens de servico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,13 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { FormBuilder } from '@angular/forms';
 import { DatabaseService } from './services/database.service';
 
+import { DatePicker } from '@ionic-native/date-picker/ngx';
+import { DatePipe } from '@angular/common';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule, IonicStorageModule.forRoot()],
-  providers: [ FormBuilder, DatabaseService, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [ FormBuilder, DatabaseService, DatePipe, DatePicker, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
